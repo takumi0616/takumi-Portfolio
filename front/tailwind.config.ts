88df2b43be241693ad015b5abeb918ec0d3e9ed0
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2024-01-08 16:21:13 +0900

    [feat]tailwind設定

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -8,13 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      // カスタムフォントの設定
+      fontFamily: {
+        sans: ['Zen Kurenaido', 'sans-serif'],
+      },
+      // カスタムカラーの設定
+      colors: {
+        white: '#FFFFFF',
+        black: '#1F2022',
+        gray: '#E1E1E1',
+        'gray-dark': '#4F626D',
+        'gray-light': '#C6D7E0',
+        'off-white': '#F7F9FD',
+      },
+      // カスタム背景イメージの設定
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
 }
+
 export default config
