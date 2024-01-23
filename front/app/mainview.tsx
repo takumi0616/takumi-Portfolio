36865c95eb6dd77577d94b07fe3b9d268635d67c
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2024-01-24 05:14:39 +0900

    [fix]背景色とメインビューアニメーション修正

--- front/app/mainview.tsx
+++ front/app/mainview.tsx
@@ -42,7 +42,7 @@ export default function MainView() {
 
     function updateAnimations() {
       const isLandscape = window.innerWidth < window.innerHeight;
-      const startTrigger = isLandscape ? 'top top' : 'center center';
+      const startTrigger = isLandscape ? 'bottom top' : 'center center';
 
       const sections = gsap.utils.toArray(
         '.content section:not(.no-animation)'
