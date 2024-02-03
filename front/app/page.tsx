396f1c9e6cced609ce0ed198b8319b54dc762347
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2024-02-03 19:55:41 +0900

    [feat]Footerの開発

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,12 +3,16 @@
 import React from 'react';
 import MainView from './mainview';
 import SubView from './subview';
+import Footer from './Footer';
 
 export default function Home() {
   return (
-    <div>
-      <MainView />
-      <SubView />
+    <div className="flex flex-col min-h-screen relative">
+      <div className="flex-grow">
+        <MainView />
+        <SubView />
+      </div>
+      <Footer />
     </div>
   );
 }
