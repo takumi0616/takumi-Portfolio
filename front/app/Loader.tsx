381ce7d8cbb5a67a70ecb0350aef43b1439b6320
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2024-02-07 00:33:59 +0900

    [fix]loading画面の改善

--- front/app/Loader.tsx
+++ front/app/Loader.tsx
@@ -1,29 +1,43 @@
 import React, { useEffect, useState } from 'react';
 
-const Loader = () => {
-  const [showText, setShowText] = useState(false);
+export const useTypingText = (text: string, speed: number) => {
+  const [textState, setTextState] = useState('');
+  const [index, setIndex] = useState(0); // 現在表示している文字のインデックス
+  const chars = text.split('');
 
   useEffect(() => {
-    const timers = [
-      setTimeout(() => setShowText(true), 1000),
-      setTimeout(() => setShowText(false), 3500),
-      setTimeout(() => {
-        const loader = document.querySelector('.loader');
-        if (loader) {
-          loader.classList.add('hidden');
-        }
-      }, 4500),
-    ];
+    if (index < chars.length) {
+      const timer = setTimeout(() => {
+        setTextState((prev) => prev + chars[index]);
+        setIndex((prevIndex) => prevIndex + 1);
+      }, speed);
+      return () => clearTimeout(timer);
+    }
+  }, [index, chars, speed]);
 
-    return () => timers.forEach(timer => clearTimeout(timer));
-  }, []);
+  return textState;
+};
+
+const Loader = () => {
+  const typedText = useTypingText('loading...', 150);
+  const [isLoadingEnd, setIsLoadingEnd] = useState(false);
+
+  useEffect(() => {
+    if (typedText === 'loading...') {
+      setTimeout(() => setIsLoadingEnd(true), 500); 
+    }
+  }, [typedText]);
 
   return (
-    <div className="loader fixed w-full h-full bg-white z-50 top-0 left-0 flex justify-center items-center"
-         style={{backgroundImage: 'linear-gradient(to right, #c9d6df 30%, #fafaff 70%)'}}>
-      <p className={`txt font-bold text-4xl color-[rgb(30,50,93)] ${showText ? 'block' : 'hidden'}`}>
-        roading ...
-      </p>
+    <div
+      className={`loader fixed w-full h-full bg-white z-50 top-0 left-0 flex justify-center items-center transition-opacity duration-1000 ${
+        isLoadingEnd ? 'opacity-0 pointer-events-none' : 'opacity-100'
+      }`}
+      style={{
+        backgroundImage: 'linear-gradient(to right, #c9d6df 30%, #fafaff 70%)',
+      }}
+    >
+      <p className="font-bold text-4xl color-[rgb(30,50,93)]">{typedText}</p>
     </div>
   );
 };
