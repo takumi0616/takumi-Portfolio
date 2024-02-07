39953e8d7321e212545d7bfdcafa63f430a44240
takasuka_takumi <s233319@stn.nagaokaut.ac.jp>
2024-02-08 00:12:37 +0900

    [fix]contributionsのgithubのsvgが表示できない場合の代替デザイン

--- front/app/Contributions.tsx
+++ front/app/Contributions.tsx
@@ -1,18 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 const Contributions: React.FC = () => {
-  const svgUrl =
-    'https://github-contributions-api.deno.dev/takumi0616.svg?no-total=true&scheme=bluegrey';
+  const [imageUrl, setImageUrl] = useState(
+    'https://github-contributions-api.deno.dev/takumi0616.svg?no-total=true&scheme=bluegrey'
+  );
+  const [isFallbackImage, setIsFallbackImage] = useState(false);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
+    const loadImage = (url: string) => {
+      const img = new Image();
+      img.onload = () => {
+        setIsFallbackImage(false);
+      };
+      img.onerror = () => {
+        setImageUrl('/github.svg');
+        setIsFallbackImage(true);
+      };
+      img.src = url;
+    };
+
+    loadImage(imageUrl);
+
     function updateAnimations() {
-      const contributionsImage = gsap.utils.toArray<Element>(
-        '.contributions-image'
-      );
+      const contributionsImage = gsap.utils.toArray<Element>('.contributions');
       contributionsImage.forEach((image) => {
         gsap.fromTo(
           image,
@@ -44,11 +58,20 @@ const Contributions: React.FC = () => {
   return (
     <div className="mb-40 text-center">
       <h2 className="text-center text-4xl mb-20">Contributions</h2>
-      <img
-        src={svgUrl}
-        alt="GitHub Contributions"
-        className="contributions-image inline-block w-full max-w-4xl"
-      />
+      <div className="contributions ">
+        <img
+          src={imageUrl}
+          alt="GitHub Contributions"
+          className={`inline-block ${
+            isFallbackImage ? 'w-8 h-8' : 'w-full max-w-4xl'
+          }`}
+        />
+        {isFallbackImage && (
+          <p className="mt-4 text-sm text-black">
+            現在、APIリクエスト制限により表示できません。
+          </p>
+        )}
+      </div>
     </div>
   );
 };
